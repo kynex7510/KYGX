ff50d53ffb2bb7f880f8223bf1b7d3afc207507c
kynex7510 <nikkitten3@gmail.com>
2026-06-19 18:47:49 +0200

    Adjust to ctr11 changes

--- Source/BatchQueue.c
+++ Source/BatchQueue.c
@@ -31,7 +31,7 @@ BatchQueueError BatchQueueInit(BatchQueue* q, size_t capacity) {
         return BatchQueueError_Success;
 
     const size_t entryOverhead = sizeof(KYGXCmd) + sizeof(size_t) + sizeof(KYGXBatchCallback) + sizeof(void*);
-    void* buffer = AllocMem(MemType_Virtual, entryOverhead * capacity);
+    void* buffer = AllocMem(MemType_Application, entryOverhead * capacity);
     if (!buffer)
         return BatchQueueError_NoMemory;
 
